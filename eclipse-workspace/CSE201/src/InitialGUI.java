5790cfd84f70f7c7095b31651c96677c0b60f94a
LordKhorne <musselad@miamioh.edu>
2022-02-27 21:38:34 -0500

    SkeletonFrame

--- eclipse-workspace/CSE201/src/InitialGUI.java
+++ eclipse-workspace/CSE201/src/InitialGUI.java
@@ -1,6 +1,35 @@
+import javax.swing.JFrame;
 
-public class InitialGUI {
 
+public class InitialGUI extends JFrame {
+
+		
+	public static void main(String[] args) {
+				
+		try {
+			
+			InitialGUI frame = new InitialGUI();
+			frame.setVisible(true);
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			
+		}
+				
+	}
 	
-	
+
+
+		/**
+		 * Create the frame.
+		 */
+		public InitialGUI() {
+			
+			setTitle("Meta-Repo");
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setBounds(100, 100, 800, 600);
+			
+	}
+
 }
