3c67815bd3a8037bddf81cf0922c31cf3ee1fce0
stafa21 <64749679+stafa21@users.noreply.github.com>
2022-03-17 06:15:40 -0400

    added search function algorithm

---
+++ eclipse-workspace/CSE201/src/search_feature.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class search_feature {
+	public static void main(String[] args) {
+		String[] apps = { "jump", "link", "Facebook", "Bumble", "tinder", "hootsuite", "James", "jim"
+
+		};
+		
+		ArrayList<String> matches = new ArrayList<String>();
+		
+		System.out.print("Search: ");
+		searchFeature(apps, matches);
+
+	}
+	
+
+	public static void searchFeature(String[] arr, ArrayList<String> match) {
+
+		Scanner input = new Scanner(System.in);
+
+		String appName = input.next();
+		
+		
+	
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].equalsIgnoreCase(appName)|| (arr[i].startsWith(appName)) || (arr[i].toLowerCase().startsWith(appName) )) {
+				match.add(arr[i]);
+
+		
+			}
+		}
+
+		for (String str : match) {
+			System.out.print(str + " ");
+		}
+
+	}
+
+}
